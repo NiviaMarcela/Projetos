b003aa20687e76102b738d1315e171e3df9e3d25
NiviaMarcela <nivhmarcelhy@hotmail.com>
2025-05-02 12:34:49 -0300

    4

--- Script.js/script.js
+++ Script.js/script.js
@@ -5,3 +5,10 @@ console.log("hello World!");
 console.log("MAURICIO");
 console.log(`ALVARO`);
 console.log("DIAS");
+
+if( 2==3) {
+    console.log("verdadeiro")
+}
+else { console.log("Falso")}
+
+    
